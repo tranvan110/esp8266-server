c4f06449ccf68400023371fa5556cabdb0c6ec03
tranvan110 <tranvan456@gmail.com>
2019-11-22 06:43:45 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const PORT = process.env.PORT || 5000;
+var ip = require('ip');
+var server = require('http').createServer();
+var io = require('socket.io')(server);
+server.listen(PORT, function () {
+    console.log("Server address: " + ip.address() + ":" + PORT);
+});
+
+var events = ["connect", "info", "getInfo", "setInfo", "message", "heartRate", "stepCount", "grygo", "control"];
+
+io.on("connection", function (socket) {
+    console.log("Connected");
+    socket.emit("ServerSent", "Send from Server");
+
+    events.forEach(function (event) {
+        socket.on(event, function (message) {
+            console.log(event + ": " + message);
+            socket.broadcast.emit(event, message);
+        });
+    });
+
+    socket.on("disconnect", function () {
+        console.log("Disconnect");
+    });
+});
